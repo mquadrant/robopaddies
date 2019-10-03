ab14706fe978f27a828d9469dfc34dfcaeddafb2
Benjamin Mark <mquadrant@gmail.com>
2019-10-03 11:00:17 +0100

    created cardlist component

---
+++ src/components/CardList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Card'
+
+export default function CardList(props) {
+    const {robots} = props;
+    return (
+        <div className="mt-8 container mx-auto flex flex-wrap justify-center">
+            {robots.map((robot,index)=>{
+                return (<Card key={index} id= {robot.id} name={robot.name} email={robot.email}/>)
+            })}
+      </div>
+    )
+}
