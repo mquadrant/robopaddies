0fdd20963260448b043f7081540bc97b0e91b0b3
Benjamin Mark <mquadrant@gmail.com>
2019-10-04 08:54:51 +0100

    added reducer to request for robots

--- src/redux/reducers.jsx
+++ src/redux/reducers.jsx
@@ -1,4 +1,7 @@
-import {CHANGE_SEARCH_FIELD} from './types'
+import {CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_PENDING, 
+    REQUEST_ROBOTS_ERROR} from './types'
 
 //Single source of truth
 //State is read only
@@ -14,6 +17,37 @@ switch(action.type){
         return {
             ...state,
             searchField: action.payload
+        }
+    default:
+        return state
+}
+}
+
+
+const initialStateRobots = {
+    isPending:false,
+    robots:[],
+    error:null
+}
+
+export const requestRobots = (state=initialStateRobots,action={})=>{
+switch(action.type){
+    case REQUEST_ROBOTS_SUCCESS:
+        return {
+            ...state,
+            robots: action.payload,
+            isPending: false
+        }
+    case REQUEST_ROBOTS_PENDING:
+        return {
+            ...state,
+            isPending: true
+        }
+    case REQUEST_ROBOTS_ERROR:
+        return {
+            ...state,
+            error: action.payload,
+            isPending: false
         }
     default:
         return state
