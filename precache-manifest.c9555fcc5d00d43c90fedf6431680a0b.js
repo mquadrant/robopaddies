42f78ae3f5a188d3d25a8c70da2ed1101be6ece4
Benjamin Mark <mquadrant@gmail.com>
2019-10-04 12:40:49 +0100

    Updates

---
+++ precache-manifest.c9555fcc5d00d43c90fedf6431680a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46407b479dd6769798ee810eece923db",
+    "url": "/robopaddies/index.html"
+  },
+  {
+    "revision": "fc5d881cfc2f02b72a3f",
+    "url": "/robopaddies/static/css/main.7adc38f2.chunk.css"
+  },
+  {
+    "revision": "784091c344f3956aa36f",
+    "url": "/robopaddies/static/js/2.f0645170.chunk.js"
+  },
+  {
+    "revision": "fc5d881cfc2f02b72a3f",
+    "url": "/robopaddies/static/js/main.5c714095.chunk.js"
+  },
+  {
+    "revision": "cc7606e2af90934c8174",
+    "url": "/robopaddies/static/js/runtime-main.0fe983dc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robopaddies/static/media/SEGA.12a3db8b.woff"
+  }
+]);
