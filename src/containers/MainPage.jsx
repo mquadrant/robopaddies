5f4f8965443ed127b55ff11f24d97ed09578cee1
Benjamin Mark <mquadrant@gmail.com>
2019-10-13 18:22:03 +0100

    created Main page container

---
+++ src/containers/MainPage.jsx
@@ -0,0 +1,51 @@
+import React ,{useState,useEffect}from 'react';
+import CardList from '../components/CardList'
+import SearchBar from '../components/SearchBar'
+import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry';
+
+
+
+export default function MainPage(props) {
+  const {requestForRobots,robots} = props;
+  const [filteredRobots, setFilteredRobots] = useState([]);
+  
+  useEffect(()=>{ 
+    requestForRobots();
+  },[requestForRobots])
+
+  useEffect(()=>{ 
+    setFilteredRobots(robots);
+  },[robots])
+
+  const searchHandler = (e)=>{ 
+    filterRobots(e.target.value);
+  }
+
+  const filterRobots = (term)=>{
+    let re = new RegExp(term, "gi");
+    setFilteredRobots(robots.filter(robot=>{
+      return re.test(robot.name) || re.test(robot.email)
+    }))
+  }
+  
+  return (
+          <div className="app-content">
+            <div className="text-center my-12">
+            <h1 className="text-5xl my-8">ROBOPADDIES</h1>
+            <SearchBar searchChange={searchHandler}/>
+            </div>
+          <hr className="border-green-200"/>
+          {filteredRobots.length?(
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots}/>
+            </ErrorBoundry>
+          </Scroll>):null}
+        </div>
+  )
+    
+}
+
+
+ 
