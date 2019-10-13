69b8a7d9d85702f8b2b8d03a00f7b5988f3ff6c6
Benjamin Mark <mquadrant@gmail.com>
2019-10-13 23:42:44 +0100

    converted to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import fetch from 'node-fetch'
 import {connect} from 'react-redux';
 import MainPage from './containers/MainPage';
 
 import {requestRobots} from './redux/action';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state:any) => {
   return {
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending
   }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch:any )=> {
  return {
-  requestForRobots: () => dispatch(requestRobots())
+  requestForRobots: () => dispatch(requestRobots(fetch))
 }
 }
 
-function App(props) {
+function App(props:any) {
   return (
     <MainPage {...props}/>
   )
