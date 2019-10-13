da59d62b8e9c2ff0d92caa20949a2acd496405bf
Benjamin Mark <mquadrant@gmail.com>
2019-10-13 18:20:51 +0100

    created test cases for card component

---
+++ src/components/__tests__/card.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow} from 'enzyme';
+import Card from './../Card';
+
+it('expect to render the Card component', () => {
+    const wrapper = shallow(<Card />);
+    expect(wrapper.length).toEqual(1);
+  });
+
+it('expect to render Card component with snapshot', () => {
+    expect(shallow(<Card />)).toMatchSnapshot();
+  });
+
+
+// it('expect to render Card component with snapshot', () => {
+//     const wrapper = shallow(<Card />);
+//     wrapper.find('[id="counter"]').simulate('click');
+//     expect(wrapper.state()).toEqual({count:1});
+//   });
+
